d5632743d864d6ea9ccfed423c20377adb2eab18
hernyyan <135739853+hernyyan@users.noreply.github.com>
2025-10-08 10:40:45 +0100

    Update sgd.py

--- sgd.py
+++ sgd.py
@@ -7,14 +7,13 @@ rng = np.random.default_rng()
 t = 0.0001 #learning rate
 n = 1000000 #number of iterations
 
-stock_path = []
-option_path = []
-
 def sgd_algo(x):
+    stock_path = []
+    option_path = []
     for i in range(n):
         #z = rng.standard_normal()
         grad_step = util.util_grad(x)
         x += t * grad_step
         stock_path.append(x[0])
         option_path.append(x[1])
-    return x
+    return x, stock_path, option_path
